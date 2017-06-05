3445b93b46e582033a0ece69032fe71eaec5f7e7
akashutreja <utreja.akash@gmail.com>
2017-06-05 23:20:18 +0530

    last review

--- media.py
+++ media.py
@@ -1,13 +1,26 @@
 import webbrowser
-class Movie():
-	"""This class store movie related information"""
-	VALID_STRINGS=["G","R","RG","RG-13"]
-	def __init__(self,title,story_line,poster,movie_trailer,review):
-		self.title=title
-		self.story_line=story_line
-		self.trailer_youtube_url=movie_trailer
-		self.poster_image_url=poster
-		self.review=review
 
-	def show_trailer(self):
-		webbrowser.open(self.movie_trailer_url)
+
+class Movie():
+    """This class store movie related information"""
+
+    def __init__(self, title, story_line, poster, movie_trailer, review):
+        """this function will be called when a movie object is created
+        keyword argument:
+        self -- as a reference which called the constructor
+        title -- a movie title
+        story_line -- a movie story line
+        poster -- a movie poster
+        trailer -- a movie trailer
+        review -- a movie review
+        """
+        self.title = title
+        self.story_line = story_line
+        self.trailer_youtube_url = movie_trailer
+        self.poster_image_url = poster
+        self.review = review
+
+    def show_trailer(self):
+        """ this function will open specific trailer
+        a movie"""
+        webbrowser.open(self.movie_trailer_url)
